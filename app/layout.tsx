bbe3e8846d76a7a8c547821e59ff5a55c8c2205b
omid baharifar <baharifargit@gmail.com>
2023-08-21 13:56:36 +0330

    feat: add rtl support

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const RootLayout: FC<Props> = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang="fa" dir="rtl">
       <body>{children}</body>
     </html>
   );
