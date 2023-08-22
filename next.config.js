3effeb7ac00ca057b827246f50a8172f784d3ba6
omid baharifar <baharifargit@gmail.com>
2023-08-23 00:50:41 +0330

    feat: add backend url as valid link image to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "team-git.iran.liara.run",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
