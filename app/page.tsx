2d149d51e6310d72573e40fa954bede7583c9b12
omid baharifar <baharifargit@gmail.com>
2023-08-22 21:21:45 +0330

    sidebar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-const Home = () => {
-  return <main></main>;
+import { FC } from "react";
+import SidebarLayout from "@layouts/sidebarLayout";
+import { getBlogs, getCategories } from "@services/shortLinks";
+
+interface Props {
+  searchParams: TSearchParams;
+}
+
+const Home: FC<Props> = async ({ searchParams }) => {
+  const [getBlogsRes, getCategoriesRes] = await Promise.all([
+    getBlogs(
+      `category=${searchParams.category || ""}&search=${
+        searchParams.search || ""
+      }`
+    ),
+    getCategories(),
+  ]);
+
+  return (
+    <main>
+      <SidebarLayout categoriesList={getCategoriesRes}>
+      </SidebarLayout>
+    </main>
+  );
 };
 
 export default Home;
