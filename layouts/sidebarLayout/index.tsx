310cc394637699f8057e71ea22eabcd2a73df855
omid baharifar <baharifargit@gmail.com>
2023-08-23 00:45:46 +0330

    fix: remove rounded classes

--- layouts/sidebarLayout/index.tsx
+++ layouts/sidebarLayout/index.tsx
@@ -19,7 +19,7 @@ const SidebarLayout: FC<props> = ({ children, categoriesList }) => {
   return (
     <div className="bg-customGray-100">
       <div className="h-screen w-full grid grid-cols-5 gap-5 p-5 max-w-containerWidth mx-auto">
-        <aside className="bg-white rounded-3xl overflow-hidden flex flex-col">
+        <aside className="bg-white overflow-hidden flex flex-col">
           <SearchBar />
           <CategoriesList categoriesList={categoriesList}/>
           {isAddBlogButtonVisible && (
@@ -31,7 +31,7 @@ const SidebarLayout: FC<props> = ({ children, categoriesList }) => {
             </div>
           )}
         </aside>
-        <div className="rounded-3xl col-span-4 overflow-y-auto hideSB">
+        <div className="col-span-4 overflow-y-auto hideSB">
           {children}
         </div>
       </div>
